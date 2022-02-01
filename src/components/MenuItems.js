9814f8afa00c2d00e883d347d3c37ab0816832e8
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-02-01 14:54:08 -0600

    ✨ Starting to hook app to redux

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,12 +1,23 @@
 import { Stack } from '@twilio-paste/stack';
 import MenuItem from './MenuItem';
 
-export const MenuItems = ({ items }) => {
-  return (
-    <Stack orientation="vertical" spacing="space60">
-      {items.map((item) => (
-        <MenuItem {...item} key={item.uuid} />
-      ))}
-    </Stack>
-  );
-};
+// export const MenuItems = ({ items }) => {
+//   return (
+//     <Stack orientation="vertical" spacing="space60">
+//       {items.map((item) => (
+//         <MenuItem {...item} key={item.uuid} />
+//       ))}
+//     </Stack>
+//   );
+// };
+
+
+const MenuItems = ({ items }) => (
+  <Stack orientation="vertical" spacing="space60">
+    {items.map((item) => (
+      <MenuItem {...item} key={item.uuid} />
+    ))}
+  </Stack>
+)
+
+export default MenuItems; 
